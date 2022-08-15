9096201501d8023d6d652ab998e1c64c99a0c121
bilalozlu <bilal5071@hotmail.com>
2022-08-15 23:24:16 +0300

    cv css (section selection)

--- src/CV_components/header_tabs.js
+++ src/CV_components/header_tabs.js
@@ -1,26 +1,34 @@
+import './header_tabs.css';
 
 function HeaderTabs(props) {
 
-    return (
-      <div>
-        <p onClick = {() => props.func('About')}>
-          About
-        </p>
-        <p onClick = {() => props.func('Experience')}>
-          Experience
-        </p>
-        <p onClick = {() => props.func('Education')}>
-          Education
-        </p>
-        <p onClick = {() => props.func('SoftwareSkills')}>
-          Software Skills
-        </p>
-        <p onClick = {() => props.func('TestScores')}>
-          Test Scores
-        </p>
+  return (
+    <div className='header_tabs'>
+      <div className='title'>
+        <p>Bilal Özlü</p>
       </div>
-    );
-  }
-  
-  export default HeaderTabs;
-  
+      <div className='tabs'>
+        <button onClick = {() => props.func('About')} className={props.selected === 'About' ? 'selected' : ''}>
+          About
+        </button>
+        <button onClick = {() => props.func('Experience')} className={props.selected === 'Experience' ? "selected" : ''}>
+          Experience
+        </button>
+        <button onClick = {() => props.func('Education')} className={props.selected === 'Education' ? "selected" : ''}>
+          Education
+        </button>
+        <button onClick = {() => props.func('SoftwareSkills')} className={props.selected === 'SoftwareSkills' ? "selected" : ''}>
+          Software Skills
+        </button>
+        <button onClick = {() => props.func('TestScores')} className={props.selected === 'TestScores' ? "selected" : ''}>
+          Test Scores
+        </button>
+      </div>
+      <div className='mail'>
+        <p>bilal.ozlu@outlook.com</p>
+      </div>
+    </div>
+  );
+}
+
+export default HeaderTabs;

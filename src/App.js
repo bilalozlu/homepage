236b54509d81d57ed0f3312696be207fb4fe905f
bilalozlu <bilal5071@hotmail.com>
2022-09-02 23:55:30 +0300

    projects link(will be filled) added to homepage

--- src/App.js
+++ src/App.js
@@ -11,13 +11,16 @@ function App() {
         <a href="BilalOzlu_CV.pdf">
           See my CV in pdf
         </a>
-          <Link to={"./CV"}>See my digital interactive super uber mega CV (under construction)</Link>
+        <Link to={"./CV"}>See my digital interactive super uber mega CV (under construction)</Link>
+        <a href="https://translator.bilalozlu.com/">
+          See my projects
+        </a>
       </header>
       <footer>
         <p>
           Burası benim tarlam bilader!
         </p>
-        <img alt="scarecrow" src="korkuluk.png" width="100px"/>
+        <img alt="scarecrow" src="korkuluk.png" width="100px" />
       </footer>
     </div>
   );

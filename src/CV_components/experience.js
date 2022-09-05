ef3e5ad9b68156002238d0c65102753ea8c62d1e
bilalozlu <bilal5071@hotmail.com>
2022-09-05 22:12:33 +0300

    a bit more text added to portfolio

--- src/CV_components/experience.js
+++ src/CV_components/experience.js
@@ -8,50 +8,56 @@ function Experience() {
           HAVELSAN
         </p>
         <p className='text'>
-          Java
+          Maintaining large Java project (debugging and analysis)
+        </p>
+        <p className='text'>
+          Developing front-end application with ReactJS
         </p>
         <p className='title'>
           StartupHeroes
         </p>
         <p className='text'>
-          JS
+          Developing front-end application with VueJS & ReactJS
         </p>
         <p className='title'>
           OBSS
         </p>
         <p className='text'>
-          JS
+          Developing front-end application with ReactJS & Redux & Hooks
         </p>
         <p className='title'>
           Turna Teknoloji
         </p>
         <p className='text'>
-          JS
+          Developing mobile application with React Native & Redux
         </p>
         <p className='title'>
           FAME CRYPT
         </p>
         <p className='text'>
-          C++
+          Implementing cryptographic algorithms with C++
+        </p>
+        <p className='text'>
+          Company website with HTML/CSS
         </p>
         <p className='title'>
           Korona Software Solutions
         </p>
         <p className='text'>
-          JS
+          Developing mobile application with React Native
         </p>
         <p className='title'>
           Piksel Bilişim
         </p>
         <p className='text'>
-          PHP
+          Developing web application (PHP, Apache, MySQL, HTML/CSS)
         </p>
       </div>
       <div className='photo'>
         <img alt='profile' src="profile.jpg" />
       </div>
     </div>
-    );
+  );
 }
 
 export default Experience;
--- src/CV_components/software_skills.js
+++ src/CV_components/software_skills.js
@@ -3,26 +3,55 @@ import './cv_section.css';
 function SoftwareSkills() {
   return (
     <div className="cv_section">
-    <div>
-      <p className='title'>
-        React 
-      </p>
-      <p className='text'>
-        Developed the site you are looking at
-      </p>
-      <p className='title'>
-        C++ 
-      </p>
-      <p className='text'>
-        Used for 3 months at work & used in most of the courses during school time
-      </p>
+      <div>
+        <p className='title'>
+          React
+        </p>
+        <p className='text'>
+          Developed the site you are looking at
+        </p>
+        <p className='title'>
+          C / C++
+        </p>
+        <p className='text'>
+          Used for 3 months at work & used in most of the courses during school time
+        </p>
+        <p className='title'>
+          JavaScript
+        </p>
+        <p className='title'>
+          Python
+        </p>
+        <p className='title'>
+          Java
+        </p>
+        <p className='title'>
+          Haskell
+        </p>
+        <p className='title'>
+          React Native
+        </p>
+        <p className='title'>
+          NodeJS
+        </p>
+        <p className='title'>
+          VueJS
+        </p>
+        <p className='title'>
+          MongoDB
+        </p>
+        <p className='title'>
+          NoSQL
+        </p>
+        <p className='title'>
+          MySQL
+        </p>
+      </div>
+      <div className='photo'>
+        <img alt='profile' src="profile.jpg" />
+      </div>
     </div>
-    <div className='photo'>
-      <img alt='profile' src="profile.jpg" />
-    </div>
-  </div>
-    );
-  }
-  
-  export default SoftwareSkills;
-  
+  );
+}
+
+export default SoftwareSkills;

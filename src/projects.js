f02a3949cf838a320911b0d4b4cbd44570816a4b
bilalozlu <bilal5071@hotmail.com>
2022-09-07 07:26:40 +0300

    new project title added

--- src/projects.js
+++ src/projects.js
@@ -6,6 +6,7 @@ function Projects() {
         <a className='projectLink' href="https://translator.bilalozlu.com/">English to Turkish translator</a>
         <a className='projectLink' href="https://bilals-invention-store.herokuapp.com">Invention-Store-App</a>
         <a className='projectLink' href="https://bilals-park-space.herokuapp.com/">Ispark-Otopark-Sorgulama</a>
+        <a className='projectLink' href="https://senior.ceng.metu.edu.tr/">METU Graduation Project (Presentation Page)</a>
         <a className='githubLink' href="https://github.com/bilalozlu"> For more, please see my github since not all projects are deployed</a>
     </div>
   );

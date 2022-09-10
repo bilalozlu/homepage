9cb239af4ed160e466d387b5702bf8b92ec53d49
bilalozlu <bilal5071@hotmail.com>
2022-09-10 12:26:58 +0300

    designed skills section with better ux

--- src/CV_components/software_skills.js
+++ src/CV_components/software_skills.js
@@ -1,54 +1,85 @@
 import './cv_section.css';
+import React, { useState } from 'react';
 
 function SoftwareSkills() {
+  const [isHoverOn, setIsHoverOn] = useState(-1);
+  const skillData = [
+    {
+      skillName: "JavaScript",
+      description: ["2.5 years of expereince in industry", "Frontend of graduation project", "Developed 'bilalozlu.com'",
+        "Mostly used with frameworks & libraries", "Using actively", "Following latest updates"]
+    },
+    {
+      skillName: "React",
+      description: ["2 years of expereince in industry", "Frontend of graduation project", "Developed 'bilalozlu.com'",
+        "Used Redux", "Used hooks", "Developement with both class and functional components",
+        "Following best practices", "Using actively", "Following latest updates"]
+    },
+    {
+      skillName: "Java",
+      description: ["asd"]
+    },
+    {
+      skillName: "C / C++",
+      description: ["asd"]
+    },
+    {
+      skillName: "NodeJS",
+      description: ["asd"]
+    },
+    {
+      skillName: "VueJS",
+      description: ["asd"]
+    },
+    {
+      skillName: "React Native",
+      description: ["asd"]
+    },
+    {
+      skillName: "Python",
+      description: ["asd"]
+    },
+    {
+      skillName: "Haskell",
+      description: ["asd"]
+    },
+    {
+      skillName: "NoSQL",
+      description: ["asd"]
+    },
+    {
+      skillName: "MongoDB",
+      description: ["asd"]
+    },
+    {
+      skillName: "MySQL",
+      description: ["asd"]
+    }
+  ]
+
   return (
     <div className="cv_section">
       <div>
-        <p className='title'>
-          React
-        </p>
-        <p className='text'>
-          Developed the site you are looking at
-        </p>
-        <p className='title'>
-          C / C++
-        </p>
-        <p className='text'>
-          Used for 3 months at work & used in most of the courses during school time
-        </p>
-        <p className='title'>
-          JavaScript
-        </p>
-        <p className='title'>
-          Python
-        </p>
-        <p className='title'>
-          Java
-        </p>
-        <p className='title'>
-          Haskell
-        </p>
-        <p className='title'>
-          React Native
-        </p>
-        <p className='title'>
-          NodeJS
-        </p>
-        <p className='title'>
-          VueJS
-        </p>
-        <p className='title'>
-          MongoDB
-        </p>
-        <p className='title'>
-          NoSQL
-        </p>
-        <p className='title'>
-          MySQL
-        </p>
+        {skillData.map((data, index) =>
+          <>
+            <p className='title' onMouseEnter={() => {
+              setIsHoverOn(index);
+            }}
+              onMouseLeave={() => {
+                setIsHoverOn(-1);
+              }}>
+              {data.skillName}
+            </p>
+            {data.description.map(oneSkill =>
+              <p className='text' style={isHoverOn === index ? { display: "block" } : { display: "none" }}>
+                {'\u2022'} {oneSkill}
+              </p>
+            )}
+          </>
+        )}
       </div>
       <div className='photoArea'>
-        <img alt='skills' src="skills.jpg" width="200px" className='photo'/>
+        <img alt='skills' src="skills.jpg" width="200px" className='photo' />
       </div>
     </div>
   );

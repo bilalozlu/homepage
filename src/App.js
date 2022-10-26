096a6cea1204adacef2ca23b69d14f51b3feff16
bilalozlu <bilal5071@hotmail.com>
2022-10-27 00:54:44 +0300

    design upgrade on main page

--- src/App.js
+++ src/App.js
@@ -5,24 +5,32 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h2>
+        <h1>
           Bilal ÖZLÜ
-        </h2>
+        </h1>
         <Link to={"./portfolio"}>Portfolio</Link>
         <a href="BilalOzlu_resume.pdf">
           Resume
         </a>
         <Link to={"./projects"}>Projects</Link>
-        <a href="/">
-          Blogs (coming soon)
-        </a>
+        <div className='links'>
+          <a className="link" href="https://www.linkedin.com/in/bilalozlu/">
+            <img src="linkedin_bw.png" alt="linkedin" width="40px" />
+            <img src="linkedin.png" alt="linkedin" width="40px" />
+          </a>
+          <a className="link" href="https://github.com/bilalozlu/">
+            <img src="github_bw.png" alt="github" width="40px" />
+            <img src="github.png" alt="github" width="40px" />
+          </a>
+          <a className="link" href="https://www.hackerrank.com/bilalozlu/">
+            <img src="hackerrank_bw.png" alt="hackerrank" width="40px" />
+            <img src="hackerrank.png" alt="hackerrank" width="40px" />
+          </a>
+        </div>
+
       </header>
       <footer>
-        <p>
-          Welcome to my land, fellow
-        </p>
-        <img alt="scarecrow" src="korkuluk.png" width="100px" />
-        <p className='copyright'>Designed and developed by Bilal ÖZLÜ {'\u00a9'} 2022</p>
+        <p className='copyright'>Designed & developed & photographed by Bilal ÖZLÜ {'\u00a9'} 2022</p>
       </footer>
     </div>
   );

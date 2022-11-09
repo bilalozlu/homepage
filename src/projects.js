20c29b162b51725099dc9d90fa9e740d84bc1369
bilalozlu <bilal.ozlu@outlook.com>
2022-11-09 21:33:17 +0300

    small update on projects page

--- src/projects.js
+++ src/projects.js
@@ -9,7 +9,7 @@ function Projects() {
         <a className='projectLink' href="https://bilals-park-space.herokuapp.com">Ispark-Otopark-Sorgulama</a>
         <a className='projectLink' href="https://senior.ceng.metu.edu.tr/2019/lpwan">METU Graduation Project (Presentation Page)</a>
         </div>
-        <a className='githubLink' href="https://github.com/bilalozlu"> More will come soon</a>
+        <p className='githubLink'> More will come soon...</p>
     </div>
   );
 }

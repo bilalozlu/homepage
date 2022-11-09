80799c6653ab0f5462a1b039974ae305a2e104f1
bilalozlu <bilal.ozlu@outlook.com>
2022-11-09 22:20:58 +0300

    solutions page started

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import './index.css';
 import App from './App';
 import Portfolio from './portfolio';
 import Projects from './projects';
+import Solutions from './solutions';
 
 import reportWebVitals from './reportWebVitals';
 
@@ -16,6 +17,7 @@ root.render(
           <Route path="/" element={<App/>}/>
           <Route path="/portfolio" element={<Portfolio/>}/>
           <Route path="/projects" element={<Projects/>}/>
+          <Route path="/solutions" element={<Solutions/>}/>
         </Routes>
     </BrowserRouter>
   </React.StrictMode>
---
+++ src/solutions.js
@@ -0,0 +1,52 @@
+import './solutions.css'
+
+function Projects() {
+    const solutionsList = [
+        {
+            problem: "hello world",
+            solution: "çözüm1",
+            links: ["stck.com", "g3.com"],
+            tags: ["java", "new project"],
+            date: "29.10.1994"
+        },
+        {
+            problem: "deneme",
+            solution: "çözüm2",
+            links: ["stck.com", "g3.com"],
+            tags: ["netlify", "new link"],
+            date: "11.11.2022"
+        }
+    ]
+    return (
+        <div className="solutions">
+            <h2 className='solutionsTitle'>
+                The list of problems I encountered during coding and how did I fix them
+            </h2>
+            <div>
+                <p>SEARCH</p>
+            </div>
+            {solutionsList.map((data) =>
+                <div className='solutionsList'>
+                    <p>
+                        {data.problem}
+                    </p>
+                    <p>
+                        {data.solution}
+                    </p>
+                    {
+                        data.links.map(link =>
+                            <p>{link}</p>
+                        )
+                    }
+                    {
+                        data.tags.map(tag =>
+                            <p>{tag}</p>
+                        )
+                    }
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default Projects;

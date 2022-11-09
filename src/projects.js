7e1fe7171b491043105b35c57af7094d6725f789
bilalozlu <bilal.ozlu@outlook.com>
2022-11-09 21:29:29 +0300

    projects page updated

--- src/projects.js
+++ src/projects.js
@@ -3,11 +3,13 @@ import './projects.css'
 function Projects() {
   return (
     <div className="projects">
+      <div className="projectsList">
         <a className='projectLink' href="https://translator.bilalozlu.com/">English to Turkish translator</a>
         <a className='projectLink' href="https://bilals-invention-store.herokuapp.com">Invention-Store-App</a>
         <a className='projectLink' href="https://bilals-park-space.herokuapp.com">Ispark-Otopark-Sorgulama</a>
         <a className='projectLink' href="https://senior.ceng.metu.edu.tr/2019/lpwan">METU Graduation Project (Presentation Page)</a>
-        <a className='githubLink' href="https://github.com/bilalozlu"> For more, please see my github since not all projects are deployed</a>
+        </div>
+        <a className='githubLink' href="https://github.com/bilalozlu"> More will come soon</a>
     </div>
   );
 }

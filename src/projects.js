a6d697bf47b031f2a98151ea5f23c37baf70c5c8
bilalozlu <bilal.ozlu@outlook.com>
2023-02-19 13:51:03 +0300

    project links updated

--- src/projects.js
+++ src/projects.js
@@ -5,8 +5,8 @@ function Projects() {
     <div className="projects">
       <div className="projectsList">
         <a className='projectLink' href="https://translator.bilalozlu.com/">English to Turkish translator</a>
-        <a className='projectLink' href="https://bilals-invention-store.herokuapp.com">Invention-Store-App</a>
-        <a className='projectLink' href="https://bilals-park-space.herokuapp.com">Ispark-Otopark-Sorgulama</a>
+        <a className='projectLink' href="https://github.com/bilalozlu/Invention-Store-App">Invention-Store-App</a>
+        <a className='projectLink' href="https://github.com/bilalozlu/Ispark-Otopark-Sorgulama">Ispark-Otopark-Sorgulama</a>
         <a className='projectLink' href="https://senior.ceng.metu.edu.tr/2019/lpwan">METU Graduation Project (Presentation Page)</a>
         </div>
         <p className='githubLink'> More will come soon...</p>

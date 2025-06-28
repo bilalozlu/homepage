0bbc71f97e79e030a3519496a691336d1c11f249
bilalozlu <bilal.ozlu@outlook.com>
2025-06-29 00:29:44 +0300

    small portfolio title change

--- src/portfolio_components/about.js
+++ src/portfolio_components/about.js
@@ -8,16 +8,7 @@ function About() {
           Bilal Özlü
         </p>
         <p className='description'>
-          A software engineer specialized on React & JS
-        </p>
-        <p className='description'>
-          loves to code
-        </p>
-        <p className='description'>
-          eager to learn
-        </p>
-        <p className='description'>
-          excited to implement new ideas
+          Full-stack javascript engineer
         </p>
       </div>
     </div>

3d69e8781a3597a45193a342cb30852459f982ae
bilalozlu <bilal.ozlu@outlook.com>
2025-06-29 11:23:11 +0300

    medium link added

--- src/App.js
+++ src/App.js
@@ -12,17 +12,25 @@ function App() {
             Resume
           </a>
         </div>
+        <div className='profile'>
+          <img src="profile.jpeg" alt="profile" width="150px" />
+        </div>
         <h1>
           <Link to={"./portfolio"}>Bilal ÖZLÜ</Link>
         </h1>
+        <p>Full-stack javascript engineer</p>
         <div className='links2'>
           <a className="link" href="https://www.linkedin.com/in/bilalozlu/">
             <img src="linkedin_bw.png" alt="linkedin" width="40px" />
             <img src="linkedin.png" alt="linkedin" width="40px" />
           </a>
           <a className="link" href="https://github.com/bilalozlu/">
-            <img src="github_bw.png" alt="github" width="40px" />
-            <img src="github.png" alt="github" width="40px" />
+            <img src="github.png" alt="github" width="42px" />
+            <img src="github.png" alt="github" width="42px" />
+          </a>
+          <a className="link" href="https://www.medium.com/@bilalozlu/">
+            <img src="medium.png" alt="medium" width="41px" />
+            <img src="medium.png" alt="medium" width="41px" />
           </a>
           <a className="link" href="https://www.hackerrank.com/bilalozlu/">
             <img src="hackerrank_bw.png" alt="hackerrank" width="40px" />

343ec690a6bcc0c55394f9c082a0de7fd51d95b9
Bilal Özlü <37140763+bilalozlu@users.noreply.github.com>
2025-07-05 23:07:33 +0300

    updated footer link area

--- src/App.js
+++ src/App.js
@@ -33,8 +33,8 @@ function App() {
       <Projects />
       <Education />
       <SoftwareSkills />
-      <a href="BilalOzlu_resume.pdf">Resume</a>
       <hr/>
+      <a href="BilalOzlu_resume.pdf">Resume</a> |
       <Link to={"./solutions"}>solutions </Link>
     </div>
   );

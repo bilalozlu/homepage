913046106ad8be9a06467edfee382c9d00719d6d
bilalozlu <bilal.ozlu@outlook.com>
2025-07-20 00:47:47 +0300

    experience info updated

--- src/my-portfolio/Experience.jsx
+++ src/my-portfolio/Experience.jsx
@@ -6,42 +6,42 @@ const experienceData = [
     date: "05.2023 – ongoing",
     position: "Software Engineer",
     logo: "./yte_logo.svg",
-    description: ["Developing a large-scaled web app(tax system of Türkiye) with NextJS, TypeScript on the front, Java at the back", "Managing a core front-end library where most components based on MUI"]
+    description: ["Developing a large-scale web application (e-Beyan: Türkiye’s national tax system) using Next.js and TypeScript (Java backend)", "Building and managing a shared front-end library used across multiple teams, primarily based on MUI"]
   },
   {
     companyName: "HAVELSAN",
     date: "02.2022 – 05.2023 (1 year, 4mo)",
     position: "Software Engineer",
     logo: "./havelsan_logo.svg",
-    description: ["Developed a front-end application with ReactJS, runs on both desktop and browser, in order to modernize an old project ", "Worked with a team on a large Java & Spring project to fulfill the requirements for acceptance tests", "Managed software requirements on DOORS & JIRA"]
+    description: ["Modernized a legacy system by building a cross-platform ReactJS frontend (desktop & browser)", "Collaborated on a large-scale Java & Spring defense project to meet acceptance test requirements", "Managed software requirements using DOORS and JIRA"]
   },
   {
     companyName: "StartupHeroes",
     date: "10.2021 – 01.2022 (4 mo)",
     position: "Software Engineer",
     logo: "./startupheroes_logo.png",
-    description: ["Developed front-end application with VueJS", "Unit testing with Jest", "Used TypeScript with ReactJS"]
+    description: ["Contributed to the development of a large-scale e-commerce app (Trendyol) using VueJS", "React (TypeScript) frontend, tested with Jest"]
   },
   {
     companyName: "OBSS",
     date: "09.2020 – 09.2021 (1 year, 1 mo)",
     position: "Software Engineer",
     logo: "./obss_logo.png",
-    description: ["Developed front-end application with ReactJS & Redux & Hooks", "Worked in a front-end team that applies scrum methodologies"]
+    description: ["Developed an interactive map-based frontend using React (Redux, Hooks) as part of a Scrum-based team for a defense project (ASELSAN SIPER)"]
   },
   {
     companyName: "Turna Teknoloji",
     date: "11.2019 – 06.2020 (7 mo)",
     position: "Part-time Software Engineer",
     logo: "./turna_logo.png",
-    description: ["Developed mobile application with React Native & Redux", "Used NodeJS with Firebase", "Unit testing with Jest"]
+    description: ["Mobile app with React Native (Class components, Redux, Jest)", "Node.js + Firebase backend"]
   },
   {
     companyName: "FAME CRYPT",
     date: "09 – 11.2019 (3 mo)",
     position: "Part-time Software Engineer",
     logo: "./famecrypt_logo.png",
-    description: ["Implemented cryptographic algorithms with C++", "Developed desktop application with Qt (C++)", "Built company’s website with HTML/CSS"]
+    description: ["Cryptography & desktop app in C++ (Qt)", "Static website development (HTML/CSS)"]
   }
 ]
 

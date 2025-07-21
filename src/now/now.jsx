efe0f104aad1c1b543b084a53a497c5a0859735f
bilalozlu <bilal.ozlu@outlook.com>
2025-07-21 22:40:28 +0300

    now page is designed

--- src/now/now.jsx
+++ src/now/now.jsx
@@ -5,9 +5,12 @@ function Now() {
     return (
         <div className="now">
             <ThemeToggle />
-            <h2>
-                Now page
-            </h2>
+            <a className="nameTitle" href="../"> ◁ Bilal ÖZLÜ</a>
+            <h2>Now</h2>
+            <p>{'\u2022'}asd</p>
+            <p>{'\u2022'}qwe</p>
+            <p>{'\u2022'}zxc</p>
+            <p className="footerText"><i>Inspired by Derek Siver's nonnownow. Last updated July 2025.</i></p>
         </div>
     );
 }

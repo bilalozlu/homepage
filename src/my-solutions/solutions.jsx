1fe076e0ae2f2760643930f20efcd75d2e41f334
bilalozlu <bilal.ozlu@outlook.com>
2025-07-23 23:42:47 +0300

    soultions page designed #1

--- src/my-solutions/solutions.jsx
+++ src/my-solutions/solutions.jsx
@@ -1,54 +1,42 @@
-import './solutions.css'
 import ThemeToggle from "../theme-toggle/ThemeToggle";
 
 function Solutions() {
-    const solutionsList = [
-        {
-            problem: "hello world",
-            solution: "çözüm1",
-            links: ["stck.com", "g3.com"],
-            tags: ["java", "new project"],
-            date: "29.10.1994"
-        },
-        {
-            problem: "deneme",
-            solution: "çözüm2",
-            links: ["stck.com", "g3.com"],
-            tags: ["netlify", "new link"],
-            date: "11.11.2022"
-        }
-    ]
-    return (
-        <div className="solutions">
-            <ThemeToggle />
-            <h2 className='solutionsTitle'>
-                Solutions
-            </h2>
-            <div>
-                <p>SEARCH</p>
-            </div>
-            {solutionsList.map((data) =>
-                <div className='solutionsList'>
-                    <p>
-                        {data.problem}
-                    </p>
-                    <p>
-                        {data.solution}
-                    </p>
-                    {
-                        data.links.map(link =>
-                            <p>{link}</p>
-                        )
-                    }
-                    {
-                        data.tags.map(tag =>
-                            <p>{tag}</p>
-                        )
-                    }
-                </div>
-            )}
+  const solutionsList = [
+    {
+      problem: "Couldn't call a function by storage event",
+      solution: "solution will be here soon",
+      links: ["https://stackoverflow.com/questions/35865481/storage-event-not-firing"],
+      tags: "#javascript #localStorage #event",
+      date: "24.07.2025"
+    },
+  ]
+  return (
+    <div className="solutions">
+      <ThemeToggle />
+      <a className="nameTitle" href="../"> ◁ Bilal ÖZLÜ</a>
+      <h2>Solutions</h2>
+      {solutionsList.map((data) =>
+        <div className='solutionsList'>
+          <p>
+            <b>Problem definition:</b> {data.problem}
+          </p>
+          <p>
+            <b>Solution:</b> {data.solution}
+          </p>
+          {
+            data.links.map((link, idx) =>
+              <span>
+                <b>Source{idx+1}:</b> <a style={{color: '#ff4d5a'}} href={link}>{link}</a>
+              </span>
+            )
+          }
+          <p>{data.date}</p>
+          <p>{data.tags}</p>
+          <hr/>
         </div>
-    );
+      )}
+    </div>
+  );
 }
 
 export default Solutions;

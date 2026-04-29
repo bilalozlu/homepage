d4bfaff1b4ed34802955d07f63d66f387b6d777b
bilal.ozlu <bilal.ozlu@tubitak.gov.tr>
2026-04-29 19:24:15 +0300

    now page update (April 2026)

--- src/now/now.jsx
+++ src/now/now.jsx
@@ -7,12 +7,14 @@ function Now() {
             <ThemeToggle />
             <a className="nameTitle" href="../"> ◁ Bilal ÖZLÜ</a>
             <h2>Now</h2>
-            <p>{'\u2022'} Working as a front-end software engineer at TÜBİTAK, in Ankara</p>
-            <p>{'\u2022'} Rediscovering the World with my 2 year old daughter</p>
-            <p>{'\u2022'} Reading 'You Don't Know JS Yet' series</p>
-            <p>{'\u2022'} Striving to maintain a daily GitHub commit streak</p>
+            <p>{'\u2022'} Working as a full-stack software engineer at TÜBİTAK, in Ankara </p>
+            <p>{'\u2022'} Rediscovering the World with my 3 year old daughter </p>
+            <p>{'\u2022'} Reading 'Effective Java' </p>
+            <p>{'\u2022'} Striving to get Meta front-end developer professional certificate </p>
+            <p>{'\u2022'} Trying to figure out how to use AI agents most effectively in a safe manner </p>
+            <p>{'\u2022'} Also, trying to figure out how will be the software development in the future </p>
             <p className="footerText">
-                <i>Inspired by Derek Siver's <a href="https://nownownow.com/about" style={{color: '#ff4d5a'}}>now</a> page movement. Last updated: July 2025.</i>
+                <i>Inspired by Derek Siver's <a href="https://nownownow.com/about" style={{color: '#ff4d5a'}}>now</a> page movement. Last updated: April 2026.</i>
             </p>
         </div>
     );
